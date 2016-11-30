937df92b845f595ce868c0dcfe0bd37f77965e57
Nico Landa <nico.aratech@gmail.com>
2016-11-30 16:31:41 +0100

    Añadidos azules corporativos

--- Utils/Color.swift
+++ Utils/Color.swift
@@ -14,6 +14,11 @@ public struct Color {
     }
     
     public struct Blue {
+        
+        public static let a = UIColor(red: 68.0/255.0, green: 116.0/255.0, blue: 177.0/255.0, alpha: 1.0)
+        public static let b = UIColor(red: 69.0/255.0, green: 145.0/255.0, blue: 199.0/255.0, alpha: 1.0)
+        public static let c = UIColor(red: 68.0/255.0, green: 195.0/255.0, blue: 234.0/255.0, alpha: 1.0)
+        
         public static let facebook = UIColor(red: 44.0/255.0, green: 101.0/255.0, blue: 148.0/255.0, alpha: 1.0)
         public static let email = UIColor(red: 52.0/255.0, green: 96.0/255.0, blue: 164.0/255.0, alpha: 1.0)
         public static let discount = UIColor(red: 30.0/255.0, green: 230.0/255.0, blue: 129.0/255.0, alpha: 1.0)
