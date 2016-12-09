b56759239a8048e4749246926c447b562062494f
Nico Landa <nico.aratech@gmail.com>
2016-12-09 13:51:37 +0100

    Swift 3.0 naming conventions

--- Utils/UIViewController.swift
+++ Utils/UIViewController.swift
@@ -8,7 +8,7 @@ import UIKit
 //MARK:- UIViewController
 
 public extension UIViewController {
-    public var contentViewController: UIViewController {
+    public var content: UIViewController {
         if let navcon = self as? UINavigationController {
             return navcon.visibleViewController!
         } else {
