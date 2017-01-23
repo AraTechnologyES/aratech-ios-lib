ef51d104b864b40572c6b0ab3ca6d4d49d427cdb
machukas <machukkas@gmail.com>
2017-01-23 16:20:27 +0100

    Añadida extension UITextView

---
+++ Utils/UITextView.swift
@@ -0,0 +1,28 @@
+//
+//  UITextView.swift
+//  Utils
+
+import Foundation
+
+extension UITextView {
+    
+    
+    /// Establece el texto en el textView
+    ///
+    /// - Parameters:
+    ///   - text: Texto para el textView
+    ///   - fullHeight: Si se desea que el textView tenga la altura necesaria para mostrar todo el texto, `true` por defecto
+    ///   - heightConstraint: Restricción de la altura del textView
+    open func setTextView(text: String, fullHeight: Bool = true, heightConstraint: NSLayoutConstraint) {
+        if fullHeight {
+            heightConstraint.isActive = false
+            self.isScrollEnabled = true
+            self.text = text
+            self.sizeToFit()
+        } else {
+            heightConstraint.isActive = true
+            self.text = text
+        }
+        self.isScrollEnabled = false
+    }
+}
