f75c4bc1d12f746465af3bdae7b6d03467c94f61
machukas <machukkas@gmail.com>
2017-02-22 15:51:42 +0100

    Posibilidad de tintar directamente la imagen resultado

--- Utils/ImageAssets.swift
+++ Utils/ImageAssets.swift
@@ -8,18 +8,25 @@ import Foundation
 
 public protocol ImageAssetProvider {
     associatedtype ImageAssetName: RawRepresentable
-    
-    static func image(forAsset asset: ImageAssetName, selected: Bool) -> UIImage
 }
 
 extension ImageAssetProvider where ImageAssetName.RawValue == String {
     
-    public static func image(forAsset asset: ImageAssetName, selected: Bool = false) -> UIImage {
-        return UIImage(named: selected ? asset.rawValue + "_selected" : asset.rawValue)!
+    public static func image(forAsset asset: ImageAssetName, selected: Bool = false, renderingMode: UIImageRenderingMode? = nil) -> UIImage {
+        let image = UIImage(named: selected ? asset.rawValue + "_selected" : asset.rawValue)!
+        if renderingMode != nil { return image.withRenderingMode(renderingMode!) }
+        return image
     }
     
-    public static func imageView(forAsset asset: ImageAssetName, selected: Bool = false) -> UIImageView {
-        return UIImageView(image: Self.image(forAsset: asset, selected: selected))
+    public static func imageView(forAsset asset: ImageAssetName, selected: Bool = false, tintColor: UIColor? = nil) -> UIImageView {
+        let imageView: UIImageView!
+        if let tintColor = tintColor {
+            imageView = UIImageView(image: Self.image(forAsset: asset, selected: selected, renderingMode: .alwaysTemplate))
+            imageView.tintColor = tintColor
+        } else {
+            imageView = UIImageView(image: Self.image(forAsset: asset, selected: selected))
+        }
+        return imageView
     }
 }
 
