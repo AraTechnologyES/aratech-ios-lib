3294e8ce6b3271d585762313ee954da2e1213df2
machukas <machukkas@gmail.com>
2018-03-19 17:10:32 +0100

    Añadido directorio ApplicationSupport a Sandbox

--- Utils/Documents/Sandbox.swift
+++ Utils/Documents/Sandbox.swift
@@ -10,4 +10,11 @@ public struct Sandbox {
     /// Document's directory of app
     public static let Documents = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
     public static let Library = try! FileManager.default.url(for: .libraryDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+	
+	public struct ApplicationSupport {
+		public static var root: URL {
+			let appBundleID = Bundle.main.bundleIdentifier!
+			return try! FileManager.default.url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent(appBundleID, isDirectory: true)
+		}
+	}
 }
