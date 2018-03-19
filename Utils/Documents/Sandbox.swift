1b7984117a38861cb2d1575b035a418729bb20f9
machukas <machukkas@gmail.com>
2018-03-19 17:28:24 +0100

    Añadida documentación del directorio de soporte de la aplicación

--- Utils/Documents/Sandbox.swift
+++ Utils/Documents/Sandbox.swift
@@ -11,7 +11,17 @@ public struct Sandbox {
     public static let Documents = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
     public static let Library = try! FileManager.default.url(for: .libraryDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
 	
+	/**
+	Directorio para datos de soporte de la aplicación, su contenido es respaldado en iCloud.
+	
+	[File System Programming Guide]:
+	https://developer.apple.com/library/content/documentation/FileManagement/Conceptual/FileSystemProgrammingGuide/FileSystemOverview/FileSystemOverview.html#//apple_ref/doc/uid/TP40010672-CH2-SW1
+	
+	Para más información sobre el sistema de archivos [File System Programming Guide].
+	
+	*/
 	public struct ApplicationSupport {
+		/// Raiz del directorio de soporte
 		public static var root: URL {
 			let appBundleID = Bundle.main.bundleIdentifier!
 			return try! FileManager.default.url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent(appBundleID, isDirectory: true)
