fde3ebfc0a150a4fb1eac7a03cd1ba3351c2392e
machukas <machukkas@gmail.com>
2018-08-23 10:35:05 +0200

    New: UIActivityIndicatorView+Loadingg

---
+++ ATLibrary/ActivityIndicator/ActivityIndicator+Loading.swift
@@ -0,0 +1,23 @@
+//
+//  ActivityIndicator+Loading.swift
+//  ATLibrary
+//
+//  Created by Nicolás Landa on 23/8/18.
+//  
+//
+
+public extension UIActivityIndicatorView {
+	
+	/// Indicador de carga, centrado y de (40, 40)
+	///
+	/// - Parameter view: Vista en la que centrar el indicador, éste NO se añade a la vista.
+	/// - Returns: El indicador de carga
+	public class func loadingIndicator(in view: UIView) -> UIActivityIndicatorView {
+		let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
+		activityIndicator.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
+		activityIndicator.center = CGPoint(x: view.bounds.size.width / 2,
+										   y: view.bounds.size.height / 2)
+		activityIndicator.hidesWhenStopped = true
+		return activityIndicator
+	}
+}
