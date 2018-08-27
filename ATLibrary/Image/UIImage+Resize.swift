645ea94ac2a9b3ab81d80e4573adbb895fd770d6
machukas <machukkas@gmail.com>
2018-08-27 11:33:52 +0200

    New: UIImage+Resize

---
+++ ATLibrary/Image/UIImage+Resize.swift
@@ -0,0 +1,27 @@
+//
+//  UIImage+Resize.swift
+//  ATLibrary
+//
+//  Created by Nicolás Landa on 27/8/18.
+//  
+//
+
+extension UIImage {
+	
+	/// Redimensiona la imagen en base a una anchura, respeta la relación de aspecto.
+	///
+	/// - Parameter newWidth: La anchura de la nueva imagen
+	/// - Returns: La imagen redimensionada
+	func resize(width newWidth: CGFloat) -> UIImage {
+		let scale = newWidth / self.size.width
+		let newHeight = self.size.height * scale
+		let newSize = CGSize(width: newWidth, height: newHeight)
+		
+		UIGraphicsBeginImageContextWithOptions(newSize, false, 0)
+		self.draw(in: CGRect(origin: .zero, size: newSize))
+		let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
+		UIGraphicsEndImageContext()
+		
+		return resizedImage!
+	}
+}
