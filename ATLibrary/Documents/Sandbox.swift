5933dca0d0c69a0290bad04e5a9d7d94bd7a29c6
machukas <machukkas@gmail.com>
2018-09-10 10:07:38 +0200

    Fix: appBundleID static

--- ATLibrary/Documents/Sandbox.swift
+++ ATLibrary/Documents/Sandbox.swift
@@ -7,6 +7,9 @@ import Foundation
 
 // MARK:- Sandbox
 public struct Sandbox {
+	
+	private static let appBundleID = Bundle.main.bundleIdentifier!
+	
     /// Document's directory of app
     public static let Documents = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
     public static let Library = try! FileManager.default.url(for: .libraryDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
@@ -24,7 +27,6 @@ public struct Sandbox {
 		
 		/// Raiz del directorio de soporte
 		public static var root: URL {
-			let appBundleID = Bundle.main.bundleIdentifier!
 			return try! FileManager.default.url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent(appBundleID, isDirectory: true)
 		}
 		
