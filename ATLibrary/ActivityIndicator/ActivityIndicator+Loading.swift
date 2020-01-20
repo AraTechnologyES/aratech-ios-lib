9d1cd7da7ef9047afc3f5b98a89273b18532cebe
machukas <machukkas@gmail.com>
2020-01-20 11:51:37 +0100

    ActivityIndicator extension to add UIActivityIndicatorView to view. Deprecated previous version

--- ATLibrary/ActivityIndicator/ActivityIndicator+Loading.swift
+++ ATLibrary/ActivityIndicator/ActivityIndicator+Loading.swift
@@ -12,12 +12,29 @@ public extension UIActivityIndicatorView {
 	///
 	/// - Parameter view: Vista en la que centrar el indicador, éste NO se añade a la vista.
 	/// - Returns: El indicador de carga
+	@available(*, deprecated, message: "Use add(to:style:size) instead")
 	public class func loadingIndicator(in view: UIView) -> UIActivityIndicatorView {
 		let activityIndicator = UIActivityIndicatorView(style: .whiteLarge)
 		activityIndicator.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
 		activityIndicator.center = CGPoint(x: view.bounds.size.width / 2,
 										   y: view.bounds.size.height / 2)
 		activityIndicator.hidesWhenStopped = true
+		return activityIndicator
+	}
+
+	class func add(to view: UIView, style: UIActivityIndicatorView.Style = .white, size: CGSize? = nil) -> UIActivityIndicatorView {
+		let activityIndicator = UIActivityIndicatorView(style: style)
+
+		if let size = size {
+			activityIndicator.frame = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+		}
+
+		activityIndicator.center = CGPoint(x: view.bounds.size.width / 2,
+																			 y: view.bounds.size.height / 2)
+
+		activityIndicator.hidesWhenStopped = true
+		view.insertSubview(activityIndicator, at: 0)
+
 		return activityIndicator
 	}
 }
