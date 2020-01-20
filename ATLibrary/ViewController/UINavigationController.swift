8973802924067ad6ce9c65251cc92f038e0cdfa0
machukas <machukkas@gmail.com>
2020-01-20 11:55:50 +0100

    Add UINavigationController function to set title with custom font and color

--- ATLibrary/ViewController/UINavigationController.swift
+++ ATLibrary/ViewController/UINavigationController.swift
@@ -32,4 +32,31 @@ public extension UINavigationItem {
 		guard let title = title else { return }
 		self.backBarButtonItem = UIBarButtonItem(title: title, style: .plain, target: nil, action: nil)
 	}
+
+	/// Establece el título del `UINavigationItem`.
+	///
+	/// Si el ancho no es suficiente amplía a 2 lineas (máximo), a partir de ahí reduce el tamaño.
+	///
+	/// - Parameters:
+	///   - title: Título
+	///   - font: Fuente, `UIFont.systemFont(ofSize: 17, weight: .semibold)` por defecto.
+	///   - color: Color del texto
+	public func setTitle(
+		_ title: String?,
+		font: UIFont = UIFont.systemFont(ofSize: 17, weight: .semibold),
+		color: UIColor) {
+
+		guard let title = title else { return }
+		let titleLabel = UILabel(frame: titleView?.frame ?? CGRect(x: 0, y: 0, width: 150, height: 44))
+		titleLabel.minimumScaleFactor = 0.25
+		titleLabel.adjustsFontSizeToFitWidth = true
+		titleLabel.numberOfLines = 2
+		titleLabel.lineBreakMode = .byClipping
+		titleLabel.font = font
+		titleLabel.textColor = .white
+		titleLabel.text = title
+		titleLabel.textAlignment = .center
+		titleLabel.sizeToFit()
+		titleView = titleLabel
+	}
 }
